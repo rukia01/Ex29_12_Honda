e4c121e3933bbc4968a36768832cc7eb9c9f526f
rinera182@gmail.com <rinera182@gmail.com>
2021-12-16 09:01:22 +0900

    プロジェクト ファイルを追加します。

---
+++ Ex29_12_Honda/Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticFunction
+{
+    class Box
+    {
+        float width;
+        float height;
+        float depth;
+        public Box(float w, float h, float d)
+        {
+            this.width = w;
+            this.height = h;
+            this.depth = d;
+        }
+        public Box(float w = 1)
+        {
+            this.width = w;
+            this.height = w;
+            this.depth = w;
+        }
+        public float GetVolume()
+        {
+            return width * height * depth;
+        }
+        public static float GetVolume(float w, float h, float d)
+        {
+            return w * h * d;
+        }
+        public float GetSurface()
+        {
+            return (width * height + height * depth + depth * width) * 2;
+        }
+        public static float GetSurface(float w, float h, float d)
+        {
+            return (w * h + h * d + d * w) * 2;
+        }
+    }
+
+}
---
+++ Ex29_12_Honda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using StaticFunction;
+
+namespace Ex29_12_Honda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box();   //何も指定しないと1
+            Console.WriteLine($"box1の表面積={box1.GetSurface()}");
+            Console.WriteLine($"box1の体積={box1.GetVolume()}");
+            Box box2 = new Box(2, 3, 5);   //寸法を指定
+            Console.WriteLine($"box2の表面積={box2.GetSurface()}");
+            Console.WriteLine($"box2の体積={box2.GetVolume()}");
+            //newで作ってすぐに破棄⇒処理の無駄が多い
+            Console.WriteLine($"box3の表面積={new Box(1, 3.5f, 2).GetSurface()}");
+            Console.WriteLine($"box3の体積={new Box(1, 3.5f, 2).GetVolume()}");
+            //staticのメソッドを呼び出すと無駄が無い
+            Console.WriteLine($"box3の表面積={ Box.GetSurface(1, 3.5f, 2)}");
+            Console.WriteLine($"box3の体積={Box.GetVolume(1, 3.5f, 2)}");
+        }
+    }
+}
